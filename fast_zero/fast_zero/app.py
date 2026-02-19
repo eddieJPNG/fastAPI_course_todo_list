81b41f8bb94a8934757daf571ecf3a02bdd04351
Edson <rochaedsonn12@gmail.com>
2026-02-19 12:10:46 -0300

    Add more tests on the project with the html validate

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,38 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.get('/home', status_code=200, response_class=HTMLResponse)
+def read_home():
+    return """
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+             <meta charset="UTF-8">
+            <meta content="width=device-width,
+            initial-scale=1.0">
+            <title>Document</title>
+            </head>
+
+            <style>
+                body {
+                    background-color: #f0f0f0;
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                    padding: 50px;
+                }
+            </style>
+            <body>
+             <h1>Olá Mundo!</h1>
+                 <hr>
+                 <p>teste em html web</p>
+            </body>
+            </html>
+    """
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_must_return_hello_world_and_ok():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, World!'}
+
+
+def test_nome_must_be_return_html_and_ok():
+    client = TestClient(app)
+
+    response = client.get('/home')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
