f08a86c0d33f6b23a8b29423b69f8bef1ba0f701
Edson <rochaedsonn12@gmail.com>
2026-02-21 20:33:09 -0300

    Add schemas of pydantic and a endpoint of post-add-user

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fast_zero.schemas import UserSchema
+from http import HTTPStatus
 
 app = FastAPI()
 
@@ -8,31 +10,6 @@ app = FastAPI()
 def read_root():
     return {'message': 'Hello, World!'}
 
-
-@app.get('/home', status_code=200, response_class=HTMLResponse)
-def read_home():
-    return """
-        <!DOCTYPE html>
-            <html lang="en">
-            <head>
-             <meta charset="UTF-8">
-            <meta content="width=device-width,
-            initial-scale=1.0">
-            <title>Document</title>
-            </head>
-
-            <style>
-                body {
-                    background-color: #f0f0f0;
-                    font-family: Arial, sans-serif;
-                    text-align: center;
-                    padding: 50px;
-                }
-            </style>
-            <body>
-             <h1>Olá Mundo!</h1>
-                 <hr>
-                 <p>teste em html web</p>
-            </body>
-            </html>
-    """
+@app.post('/users/', status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    return user
